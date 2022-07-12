0973d3b4bfb3b69d9c9bd98f8f0a302e25f25b68
wbajwa <wbajwa@choice.com.au>
2022-07-12 10:54:09 +1000

    - Updated logic to render correct hover color

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 let expenses  = document.querySelectorAll('[data-expense]');
 let bars = document.querySelectorAll('.bars-bar');
-console.log(expenses);
-console.log(bars);
 
+//Get the day of week with name
+const weekday = ["sun","mon","tue","wed","thu","fri","sat"];
+const d = new Date();
+let dayOfWeek = weekday[d.getDay()];
 
+//Loop through all the expenses elements
 expenses.forEach(element => {
+    //Get value of data-expense attribute
     let expense = element.dataset.expense;
-    element.style.height = `${expense*2}px`
-        console.log(element)
+    //Get value of data-day attribute
+    let day = element.dataset.day;
+    //Add inline style for height expense value multiply 2 to get higher bar
+    element.style.height = `${expense*2}px`;
+    //Check if value of the day matches with the current day of the week
+    if(day === dayOfWeek){
+        //If yes add class today to render correct  hover styling
+        element.classList.add('today');
+    }
 });
